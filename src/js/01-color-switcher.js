154b174eba6839a2e9ba7fe81a5d6d37c62ee42a
YuliiaMakhovska <y.makhovska@gmail.com>
2022-10-06 17:12:17 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+stopBtn.disabled = false;
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+}
+
+function onStopBtn() {
+  clearInterval(timerId);
+  stopBtn.disabled = false;
+  startBtn.disabled = false;
+}
